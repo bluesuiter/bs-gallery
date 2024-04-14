45b2fd5d5d4e22eeaf796ffd31d7472f94d5ab13
bluesuiter <17717751+bluesuiter@users.noreply.github.com>
2024-04-14 09:45:58 +0530

    update admin ui

--- view/gallery/create.php
+++ view/gallery/create.php
@@ -1,37 +1,68 @@
 <div class="wrap">
-    <div class="col-12 bsg_gallery">
-        <h1 class="row">Add Gallery
-            <a href="<?php echo admin_url('admin.php?page=bsg_gallery') ?>" class="ml-auto button button-primary">Galleries</a>
-        </h1>
-        <hr class="w-100"/>
+
+    <h1 class="row">Add New Gallery</h1>
+    <div id="poststuff">
         <form method="post" action="<?php echo admin_url('admin-post.php') ?>" name="create_gallery">
-            <div class="row px-3">
-                <div class="col-3">
-                    <label>Gallery Name</label>
-                    <input type="text" name="galleryName" required class="" value="Gallery <?php echo $nextId ?>"/>
+
+            <div id="post-body" class="metabox-holder columns-2">
+
+                <div id="post-body-content">
+                    <div id="titlediv">
+                        <div id="titlewrap">
+                            <label class="" id="title-prompt-text" for="title">Add gallery title</label>
+                            <input type="text" name="galleryName" size="30" value="" id="title" spellcheck="true" autocomplete="off">
+                        </div>
+                        <div class="inside">
+                            <div id="edit-slug-box" class="hide-if-no-js">
+                            </div>
+                        </div>
+                        <input type="hidden" id="samplepermalinknonce" name="samplepermalinknonce" value="2ddc17e810">
+                    </div>
+
+                    <!-- gallery-body -->
+                    <div id="glryBody" class="w-100"></div>
                 </div>
 
-                <div class="col-3">
-                    <label>Short Code</label>
-                    <span><input readonly type="text" name="shortCode" value="[bsgallery id=<?php echo $nextId ?>]" /></span>
+
+                <div id="postbox-container-1" class="postbox-container">
+                    <div id="submitdiv" class="postbox">
+                        <h2 class="hndle ui-sortable-handle">Publish</h2>
+
+                        <div class="inside">
+                            <div class="submitbox" id="submitpost">
+                                <div id="minor-publishing">
+                                    <div class="misc-pub-section">
+                                        Short Code
+                                        <span><input readonly type="text" name="shortCode" value="[bsgallery id=<?php echo $nextId ?>]" /></span>
+                                    </div>
+
+                                    <div class="misc-pub-section">
+                                        Gallery Type
+                                        <span class="d-block my-1">
+                                            <input type="radio" name="galleryType" value="image" checked /> Image
+                                            <input type="radio" name="galleryType" value="docs" /> Documents
+                                        </span>
+                                    </div>
+                                    <div class="clear"></div>
+
+                                    <div id="major-publishing-actions">
+                                        <?php wp_nonce_field('bsg_gallery', 'create_gallery') ?>
+                                        <input type="hidden" name="action" value="bsg_saveGallery" />
+                                        <button type="button" class="py-1 button button-primary multiple" onclick="bsGallery.upload(this)" name="upload_file"><i class="dashicons dashicons-upload"></i> Upload</button>
+                                        <button type="submit" class="py-1 button button-primary multiple" name="save_gallery"><i class="dashicons dashicons-saved"></i> Save Gallery</button>
+                                    </div>
+                                    <hr class="w-100" />
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
 
-                <div class="col-3">
-                    <label>Gallery Type</label>
-                    <span class="d-block my-1">
-                        <input type="radio" name="galleryType" value="image" checked /> Image
-                        <input type="radio" name="galleryType" value="docs" /> Documents
-                    </span>
-                </div>
 
-                <div class="col-3 pt-2">
-                    <?php wp_nonce_field('bsg_gallery', 'create_gallery') ?>
-                    <input type="hidden" name="action" value="bsg_saveGallery"/>
-                    <button type="button" class="py-1 button button-primary multiple" onclick="bsGallery.upload(this)" name="upload_file"><i class="dashicons dashicons-upload"></i> Upload</button>
-                    <button type="submit" class="py-1 button button-primary multiple" name="save_gallery"><i class="dashicons dashicons-saved"></i> Save Gallery</button>
-                </div>
-                <hr class="w-100"/>
-                <div id="glryBody" class="w-100"></div>
+
+
         </form>
     </div>
 </div>
--- view/gallery/index.php
+++ view/gallery/index.php
@@ -1,33 +1,29 @@
 <div class="wrap">
-    <div class="col-12">
-        <h1 class="row">Gallery
-            <a href="<?php echo admin_url('admin.php?page=bsg_createGallery') ?>" class="ml-auto button button-primary">Add Gallery</a>
-        </h1>
-        <table class="wp-list-table widefat fixed striped table-view-list">
-            <thead>
-                <tr>
-                    <th>SNo.</th>
-                    <th>Gallery Name</th>
-                    <th>Short-Code</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if(!empty($gallery)): ?>
-                    <?php foreach($gallery as $index => $item): ?>
-                        <tr>
-                            <td><?= ($index+1) ?></td>
-                            <td><?= $item['gallery_name'] ?></td>
-                            <td><?= '[bsgallery id='.$item['id'].']' ?></td>
-                            <td>
-                                <a href="<?= admin_url('admin.php?page=bsg_editGallery&id='.$item['id']) ?>" title="Edit Gallery">
-                                    <i class="dashicons dashicons-edit"></i>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
-    </div>
+    <h1 class="wp-heading-inline">BS-Gallery</h1>
+    <table class="wp-list-table widefat fixed striped table-view-list">
+        <thead>
+            <tr>
+                <th>SNo.</th>
+                <th>Gallery Name</th>
+                <th>Short-Code</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($gallery)) : ?>
+                <?php foreach ($gallery as $index => $item) : ?>
+                    <tr>
+                        <td><?= ($index + 1) ?></td>
+                        <td><?= $item['gallery_name'] ?></td>
+                        <td><?= '[bsgallery id=' . $item['id'] . ']' ?></td>
+                        <td>
+                            <a href="<?= admin_url('admin.php?page=bsg_editGallery&id=' . $item['id']) ?>" title="Edit Gallery">
+                                <i class="dashicons dashicons-edit"></i>
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
+    </table>
 </div>
